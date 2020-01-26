994adaa312bd6ec0e3940c9b18722fef51edd150
suresh528kumar <30729341+suresh528kumar@users.noreply.github.com>
2020-01-26 17:10:28 +0700

    echoing echo "Creating deployment"

--- app-deployment.sh
+++ app-deployment.sh
@@ -28,6 +28,10 @@ if [ $status_code == 200 ]; then
 else
  echo
  echo "Creating deployment"
+ echo KUBERNETES_SERVICE_HOST $KUBERNETES_SERVICE_HOST
+ echo KUBERNETES_PORT_443_TCP_PORT $KUBERNETES_PORT_443_TCP_PORT
+ echo KUBE_TOKEN $KUBE_TOKEN
+ echo "Creating deployment2"
  curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments" \
     -X POST -d @bulletin-board-deployment.json
@@ -38,7 +42,8 @@ status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     -X GET -o /dev/null -w "%{http_code}")
 
 if [ $status_code == 404 ]; then
- echo
+ 
+ echo "Creating service"
  echo "Creating service"
  curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
     "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/services" \
